9004899e4911e9db49efd856edad63ab3909f62c
chmoking <montalvo.king@gmail.com>
2025-11-07 12:14:18 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("¡Hola Mundo desde Python en Ubuntu!")
+    nombre = input("¿Cuál es tu nombre? ")
+    print(f"Mucho gusto, {nombre}.")
+
+if __name__ == "__main__":
+    main()
